c12d8a33a2f55147131bd7027e08e46f36a81b3a
Soutert <hnalvarez@unah.hn>
2025-07-29 23:21:19 -0600

    fix: mejora visual del footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { Outlet, Link } from 'react-router-dom';
-import './App.css';
-import LogoBM from './assets/imgs/LogoBM.jpg';
-
+import { Outlet, Link } from "react-router-dom";
+import "./App.css";
+import LogoBM from "./assets/imgs/LogoBM.jpg";
 
 function App() {
   return (
     <>
-
       <header className="header">
         <div className="contenedor-logo">
           <img src={LogoBM} alt="Baires Muebles Logo" className="logo" />
           <h1>Baires Muebles</h1>
-          
         </div>
         <nav className="navbar">
           <ul className="nav">
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/fabricacion">Fabricación</Link></li>
-            <li><Link to="/tapiceria">Tapicería</Link></li>
-            <li><Link to="/contacto">Contacto</Link></li>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/fabricacion">Fabricación</Link>
+            </li>
+            <li>
+              <Link to="/tapiceria">Tapicería</Link>
+            </li>
+            <li>
+              <Link to="/contacto">Contacto</Link>
+            </li>
           </ul>
         </nav>
       </header>
@@ -31,11 +36,27 @@ function App() {
 
       <footer className="footer">
         <div className="footer-contenido">
-          <p>&copy;Baires Muebles. Todos los derechos reservados.</p>
-          <p>Ubicados en Tegucigalpa, Honduras</p>
-          <p>Síguenos en:
-            <a href="https://www.facebook.com/BairesMueblesHn/" target="_blank"> Facebook</a> |
-            <a href="https://www.instagram.com/bairesmuebles/" target="_blank"> Instagram</a>
+          <p className="footer-text">
+            &copy; Baires Muebles. Todos los derechos reservados.
+          </p>
+          <p className="footer-text">Ubicados en Tegucigalpa, Honduras</p>
+          <p className="footer-text">
+            Síguenos en: 
+            <a
+              href="https://www.facebook.com/BairesMueblesHn/"
+              target="_blank"
+              className="footer-link"
+            >
+              Facebook 
+            </a>
+            <span className="separator">|</span>
+            <a
+              href="https://www.instagram.com/bairesmuebles/"
+              target="_blank"
+              className="footer-link"
+            >
+              Instagram
+            </a>
           </p>
         </div>
       </footer>
